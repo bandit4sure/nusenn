c9ab205e97699f1c100dd17d18df714d6bb4c154
bandit4sure <102635055+bandit4sure@users.noreply.github.com>
2025-05-22 20:48:52 +0000

    redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://webreview-nusen.live/home"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://nusen-webreview.live/home"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
